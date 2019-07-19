01b743acdf9ed8767fe84b23deb0830d51885e81
dhruv5201 <dhrubajitdas@gmail.com>
2019-07-19 15:54:19 +0530

    First Comming

---
+++ CoreJava/src/string/StringTest1.java
@@ -0,0 +1,13 @@
+package string;
+
+public class StringTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =5;
+		int b = 10;
+		System.out.println("Addition is : " + (a+b));
+
+	}
+
+}
