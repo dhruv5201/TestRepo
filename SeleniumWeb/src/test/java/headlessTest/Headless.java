f61579970687bbabb4e88ecf8ac88da569e9c20f
dhruv5201 <dhrubajitdas@gmail.com>
2019-09-12 14:55:07 +0530

    Karate

--- SeleniumWeb/src/test/java/headlessTest/Headless.java
+++ SeleniumWeb/src/test/java/headlessTest/Headless.java
@@ -1,4 +1,6 @@
 package headlessTest;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
@@ -13,9 +15,12 @@ public class Headless {
 	  
 	  driver.get(PropertyReader.getProperty("url"));
 	  System.out.println(driver.getTitle().toString());
+	  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	  driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Selenium HeadLess Testing..");
 	  driver.findElement(By.xpath("//div[@class='FPdoLc VlcLAe']//input[@name='btnK']")).click();
-	  
+	  driver.findElement(By.xpath("//div[contains(text(),'Selenium Headless Browser Testing: HTMLUnitDriver')]")).click();
+	  //System.out.println("Search Result is: " + searchResult);
+	  System.out.println(driver.getTitle().toString());
 	  driver.quit();
   }
 }
