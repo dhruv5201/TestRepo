65d45936cfbd629094e20f2ac2a137a0492a9844
dhruv5201 <dhrubajitdas@gmail.com>
2021-03-31 12:06:18 +0530

    Python Work

--- PythonSpace/Practice/ChildDemo.py
+++ PythonSpace/Practice/ChildDemo.py
@@ -1,12 +1,24 @@
-from Practice.OppsDemo import Calculator
+from Practice.OppsDemo import Calculator , Animal
 
 
-class ChildImplementation(Calculator):
+class ChildImplementation(Calculator, Animal):
     num3 = 400
+    def __init__(self):
+        Calculator.__init__(self,5,10)
 
     def getalldata(self):
         print(self.num3)
 
 
 obj = ChildImplementation()
-obj.getalldata()
+obj.summation()
+print("Parent Class Variable: {}".format(obj.num))
+print("Parent Class Variable: {}".format(obj.num1))
+print("Parent Class Variable: {}".format(obj.num2))
+print("Child Class Variable: {}".format(obj.num3))
+print("Animal Class method Access: ".format(obj.move()))
+print("Animal Class method Access: ".format(obj.eat()))
+
+
+
+
